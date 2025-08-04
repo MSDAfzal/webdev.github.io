7bb3d089ad7b85db6ccc010564581ddf9f873266
MSDAfzal <sindsagar@yahoo.com>
2025-08-04 09:19:16 +0500

    Add files via upload

---
+++ .github/scripts/main.js
@@ -0,0 +1,46 @@
+const listItems = document.querySelectorAll("li");
+
+function toggleDone(e) {
+  if (!e.target.className) {
+    e.target.className = "done";
+  } else {
+    e.target.className = "";
+  }
+}
+
+listItems.forEach((item) => {
+  item.addEventListener("click", toggleDone);
+});
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/logo1.jpg") {
+    myImage.setAttribute("src", "images/logo2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/logo1.jpg");
+  }
+});
+// Welcome Message
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcome to Margalla Skills Develoment Academy!, ${myName}`;
+  }
+}
+//initialization
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Welcome to Margalla Skills Develoment Academy!, ${storedName}`;
+}
+//EO initialization
+myButton.addEventListener("click", () => {
+  setUserName();
+});
